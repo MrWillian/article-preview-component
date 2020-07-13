761a2742a4d13e5bc2dc4f28df2fefcad65372c2
MrWilllian <willian.dev10@gmail.com>
2020-07-13 09:48:37 -0300

    refactor: include Share popup index file component

---
+++ src/components/SharePopup/index.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+import { 
+  DialogFormContainer, PopUpContainer, FacebookButton, TwitterButton, PinterestButton, Dialog
+} from './styles';
+
+interface Props {
+  isActive?: boolean;
+}
+
+const SharePopup: React.FC<Props> = ({ isActive }) => {
+  return (
+    <DialogFormContainer className={isActive ? 'active' : ''}>
+      <PopUpContainer>
+        <strong>Share</strong>
+        <FacebookButton />
+        <TwitterButton />
+        <PinterestButton />
+      </PopUpContainer>
+      <Dialog />
+    </DialogFormContainer>
+  );
+};
+
+export default SharePopup;
