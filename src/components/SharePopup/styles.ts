1b3c679ffb114b7a5a4fcdfac5aa9b016d89b6b8
MrWilllian <willian.dev10@gmail.com>
2020-07-16 15:42:46 -0300

    style: all responsive questions rebuilded

--- src/components/SharePopup/styles.ts
+++ src/components/SharePopup/styles.ts
@@ -2,105 +2,107 @@ import styled, { css } from "styled-components";
 
 export const DialogFormContainer = styled.div`
   display: none;
-  position: absolute;
-
-  right: 0;
-  margin-top: 25px;
-  margin-right: 15px;
-
+  
   &.active {
-    display: block;
-    transition: opacity 800ms 400ms;
-  }
+    position: absolute;
+    bottom: 8rem;
+    right: -2rem;
 
-  @media (max-width: 375px) {
-    margin-top: 70px;
-    margin-right: 0;
+    display: block;
+    transition: opacity 0.25s ease-out, background 0.25s ease-out;
+    
+    @media (max-width: 800px) {
+      bottom: 0;
+      right: 0;
+      z-index: 2;
+
+      min-width: 100%;
+      border-radius: 8px;
+    }
   }
 `;
 
 export const PopUpContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-evenly;
-  padding: 10px;
+  justify-content: space-between;
 
-  width: 160px;
-  height: 35px;
-  border-radius: 5px;
+  padding: 1.3rem 2rem;
+  border-radius: 8px;
 
   background-color: var(--very-dark-grayish-blue);
   -webkit-box-shadow: 10px 10px 18px -12px var(--desaturated-dark-blue);
   -moz-box-shadow: 10px 10px 18px -12px var(--desaturated-dark-blue);
   box-shadow: 10px 10px 18px -12px var(--desaturated-dark-blue);
 
-  position: absolute;
-  right: 0;
   z-index: 10;
 
   > strong {
     text-transform: uppercase;
-    font-size: 10px;
+    letter-spacing: 4px;
+    font-size: 0.75rem;
     color: var(--grayish-blue);
+    z-index: 11;
 
-    @media (max-width: 375px) {
+    @media (max-width: 800px) {
       font-size: 14px;
     }
   }
 
-  @media (max-width: 375px) {
-    width: 80vw;
-    height: 80px;
-    z-index: 2;
+  @media (max-width: 800px) {
+    justify-content: flex-start;
 
-    padding-right: 50px;
-    /* margin-top: 70px; */
+    padding-top: 25px;
+    padding-bottom: 25px;
 
-    border-radius: 10px;
     border-top-left-radius: 0px;
     border-top-right-radius: 0px;
   }
 `;
 
-const iconCSS = css`
+const buttonCSS = css`
   width: 15px;
   height: 15px;
+  z-index: 11;
+  margin-left: 8px;
+  margin-right: 8px;
 
   cursor: pointer;
   border: none;
   outline: none;
 
-  @media (max-width: 375px) {
+  @media (max-width: 800px) {
     width: 20px;
     height: 20px;
   }
 `;
 
 export const FacebookButton = styled.button`
-  ${iconCSS};
+  ${buttonCSS};
   background: url('images/icon-facebook.svg') round;
 `;
 
 export const TwitterButton = styled.button`
-  ${iconCSS};
+  ${buttonCSS};
   background: url('images/icon-twitter.svg') round;
 `;
 
 export const PinterestButton = styled.button`
-  ${iconCSS};
+  ${buttonCSS};
   background: url('images/icon-pinterest.svg') round;
 `;
 
 export const Dialog = styled.div`
-  border-left: 40px solid transparent; 
-  border-right: 40px solid transparent; 
-  border-top: 42px solid var(--very-dark-grayish-blue);
+  border-left: 30px solid transparent; 
+  border-right: 30px solid transparent; 
+  border-top: 30px solid var(--very-dark-grayish-blue);
 
   position: absolute;
-  right: 0;
-  top: 0;
+  top: 2.5rem;
+  right: 4rem;
+  z-index: 9;
 
-  @media (max-width: 375px) {
+  @media (max-width: 800px) {
     display: none;
   }
 `;
